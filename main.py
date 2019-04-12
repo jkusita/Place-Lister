f4c163706fc89ffda155d7706676e8fbd39d31fe
JKU <46511897+jkusita@users.noreply.github.com>
2019-04-12 14:30:42 +0800

    Added line 7

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ print("No! this IS main.c!")
 print("You suck at games! - vscode")
 print("I am main.c and I don't suck at anything!") # Lies!
 print("Burgers, fries, and ice cream boys!") 
+print("What's up!")
