98135505b8fe3a7fc2792f7802f2c8114e7fe66c
JKU <46511897+jkusita@users.noreply.github.com>
2019-04-29 22:28:12 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-print("This is main.c")
-print("No! this is vscode!")
-print("No! this IS main.c!")
-print("You suck at games! - vscode")
-print("I am main.c and I don't suck at anything!") # Lies!
-print("Burgers, fries, and ice cream boys!") 
-print("What's up!")
-print("test")
